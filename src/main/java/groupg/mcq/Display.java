d20acc87f85afb9992e516833b4452f0b940ff27
Matthew Waters <matthewwaters49@gmail.com>
2021-03-16 15:05:36 +0000

    fix unneccessary whitespace changes + import changes

--- src/main/java/groupg/mcq/Display.java
+++ src/main/java/groupg/mcq/Display.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.beans.PropertyVetoException;
 
 public class Display extends JFrame {
 
